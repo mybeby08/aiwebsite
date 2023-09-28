2df90e9efd4bb88f1fb0b7edcc0230121bcf3236
mybeby08 <cabandebeb@gmail.com>
2023-09-28 20:01:03 +0000

    added responsivness to landing footer

--- components/landing/landingcontent.tsx
+++ components/landing/landingcontent.tsx
@@ -4,8 +4,8 @@ import { Mail, Phone, Github, Linkedin } from "lucide-react";
 import { constants } from "@/constants";
 const LandingContent = () => {
   return (
-    <footer className="px-10 pb-20 w-full">
-      <div className="grid sm:grid-cols-2 md:grid-cols-4 space-x-4">
+    <footer className="px-10 pb-20">
+      <div className="grid sm:grid-cols-2 md:grid-cols-4 sm:space-x-4">
         <div className="space-y-2">
           <h1 className="text-3xl font-bold text-white">AI.betsy</h1>
           <p className="text-sm text-slate-400">
@@ -20,10 +20,10 @@ const LandingContent = () => {
             with OPENAI API
           </p>
         </div>
-        <div className="space-y-2 col-end-7 col-span-2">
+        <div className="space-y-2 sm:col-end-7 sm:col-span-2 md:col-span-1 md:col-end-5 lg:col-end-7 lg:col-span-2">
           <h1 className="text-3xl font-bold text-white">Contact</h1>
           <div className="flex flex-col gap-y-3 font-semibold">
-            <div className="grid sm:grid-cols-2 gap-x-5">
+            <div className="grid sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-x-5">
               <span className="flex">
                 <Mail className="mr-2" />
                 {constants.EMAIL}
